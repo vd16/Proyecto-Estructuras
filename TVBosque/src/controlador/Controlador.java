e4700f1875a08ba2957886386f3f182f130b7793
vd16 <victorian772@gmail.com>
2018-08-22 19:19:11 -0500

    Add files via upload

--- TVBosque/src/controlador/Controlador.java
+++ TVBosque/src/controlador/Controlador.java
@@ -1,5 +1,8 @@
 package controlador;
 
+import java.util.Iterator;
+
+import modelo.Pelicula;
 import modelo.TVBosque;
 import persistencia.AdministracionIO;
 
@@ -10,7 +13,7 @@ public class Controlador {
 	
 	public Controlador() {
 		bosque = new TVBosque();
-		admi = new AdministracionIO(bosque);
+		admi = new AdministracionIO(this);
 		
 		try
 		{
@@ -22,16 +25,44 @@ public class Controlador {
 		
 	}
 	
-	public void agregarPelicula(String titulo, String estudio, String version, String estado, int precio, String clasificacion, String anio, String genero, String fecha, String ID) {
+	public void cargarPelicula(String titulo, String estudio, String version, String estado, int precio, String clasificacion, String anio, String genero, String fecha, String ID) {
+		bosque.cargar(titulo, estudio, version, estado, precio, clasificacion, anio, genero, fecha, ID);
+	}
+	
+	public void editar(String ID, Pelicula editada) {
 		try {
-			bosque.cargar(titulo, estudio, version, estado, precio, clasificacion, anio, genero, fecha, ID);
+			bosque.editar(ID, editada);
 		}catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
 	
+	public Iterator<Pelicula> buscarPeriodoDebut(String anio) {
+		Iterator<Pelicula> it = null;
+		
+		try {
+			it = bosque.buscarPeriodoDebut(anio);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return it;
+	}
+	
+	public Iterator<Pelicula> darPeliculas() {
+		return bosque.darPeliculas();
+	}
+	
 	public static void main(String[] args) {
 		Controlador c = new Controlador();
+	}
+	
+	public void guardar() {
+		try {
+			admi.guardar();
+		} catch(Exception e) {
+			
+		}
 	}
 }
