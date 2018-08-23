03848370c41287acbc91640dacea93b9e0c8c9ec
vd16 <victorian772@gmail.com>
2018-08-22 19:18:48 -0500

    Add files via upload

--- TVBosque/src/persistencia/AdministracionIO.java
+++ TVBosque/src/persistencia/AdministracionIO.java
@@ -3,30 +3,51 @@ package persistencia;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Iterator;
 
+import controlador.Controlador;
+import modelo.Pelicula;
 import modelo.TVBosque;
 
 public class AdministracionIO {
 
-	private TVBosque bosque;
-	
-	public AdministracionIO(TVBosque bosque) {
-		this.bosque = bosque;
+	private Controlador c;
+
+	public AdministracionIO(Controlador c) {
+		this.c = c;
 	}
-	
-	public void cargar()throws Exception
-	{
+
+	public void cargar() throws Exception {
 		File file = new File("./data/DVD.txt");
 		FileReader fr = new FileReader(file);
 		BufferedReader bf = new BufferedReader(fr);
 		String linea;
-		
-		while((linea = bf.readLine()) != null) {
+
+		while ((linea = bf.readLine()) != null) {
 			String[] pelicula = linea.split(";");
-			
-			bosque.cargar(pelicula[0], pelicula[1], pelicula[2], pelicula[3], Integer.parseInt(pelicula[4]) , pelicula[5],
-						pelicula[6], pelicula[7], pelicula[8], pelicula[9]);
+
+			c.cargarPelicula(pelicula[0], pelicula[1], pelicula[2], pelicula[3], Integer.parseInt(pelicula[4]),
+					pelicula[5], pelicula[6], pelicula[7], pelicula[8], pelicula[9]);
 		}
+		
+		bf.close();
 	}
-	
+
+	public void guardar() throws Exception {
+		File file = new File("./data/DVD.txt");
+		PrintWriter pw = new PrintWriter(file);
+		
+		Iterator<Pelicula> lista = c.darPeliculas();
+		
+		while(lista.hasNext()){
+			Pelicula actual = lista.next();
+			pw.println(actual.getTitulo() + ";" + actual.getEstudio() + ";" + actual.getEstado() + ";" + actual.getVersion() + ";"
+					+ actual.getPrecio() + ";" + actual.getClasificacion() + ";" + actual.getAnio() + ";" + actual.getGenero() + ";"
+					+ actual.getFecha() + ";" + actual.getID());
+		}
+		
+		pw.close();
+	}
+
 }
